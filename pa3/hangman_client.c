32d7abf919ab48a6d3b0a7e617b3b9c580b89806
Preetham Kesineni <preethamkesineni@umail.ucsb.edu>
2019-06-06 01:49:59 -0700

    pa3

---
+++ pa3/hangman_client.c
@@ -0,0 +1,140 @@
+// Client side implementation of UDP client-server model 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <sys/time.h>
+#include <time.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <ctype.h>
+#define MAXLINE 1024 
+
+// Driver code 
+int main(int argc, char* argv[]) {
+
+	int port = atoi(argv[2]);	
+	int sockfd; 
+	char buffer[MAXLINE];  
+	struct sockaddr_in	 servaddr; 
+	struct hostent *host_entry;
+	struct timeval timeout={1,0};
+
+	// Creating socket file descriptor 
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+		perror("socket creation failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	
+
+
+	// Filling server information 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_port = htons(port); 
+	
+	if(argv[1][0] == 'c')
+	{
+		host_entry = (struct hostent*)gethostbyname(argv[1]);
+		memcpy(&servaddr.sin_addr, host_entry->h_addr_list[0], host_entry->h_length);
+	}
+
+	else{
+		servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+	}
+
+	setsockopt(sockfd,SOL_SOCKET,SO_RCVTIMEO,(char*)&timeout,sizeof(struct timeval));
+
+
+	
+	
+	//servaddr.sin_addr.s_addr = INADDR_ANY; 
+	
+	int n, len; 
+	
+
+	
+	char *message = "";
+	char word[MAXLINE];
+	char guessWord[] = "        ";
+	int gameSuccess = 0;
+	int guesses = 0;
+	int wordLength = 0;	
+	char userInput[1];
+	printf("Ready to start game? (y/n):\n");
+	scanf("%s",userInput);
+	if(strcmp(userInput,"y") == 0)
+	{
+		sendto(sockfd, (const char *)message, strlen(message),
+                MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+                        sizeof(servaddr));
+	        //printf("empty message sent.\n");
+			
+		
+                n = recvfrom(sockfd, (char *)word, MAXLINE,
+                                MSG_WAITALL, ( struct sockaddr *) &servaddr,
+                                &len);
+                word[n] = '\0';
+		wordLength = word[1] - '0';
+		//printf("server:%c \n",word[0]);
+
+
+		for(int i = 0; i < 8; i++)
+		{
+			guessWord[i] = word[i + 3];
+		}
+
+        	printf("%s\n",guessWord);
+
+		guesses = word[2] - '0';
+		gameSuccess = word[0] - '0';
+		
+		while( (guesses < 6) || (gameSuccess == 0))
+		{
+			printf("Letter to guess:");
+			scanf("%s",userInput);
+			char *guess = userInput;
+			int length = strlen(guess);
+			if(isdigit(userInput[0]) || length != 1)
+			{
+				printf("Error! Please guess one letter.\n");
+			}
+			else
+			{
+				guess[0] = tolower(guess[0]);
+				
+				sendto(sockfd, (const char *)guess, strlen(guess),
+                		MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+                        		sizeof(servaddr));
+        			//printf("Guess Sent.\n");
+				
+				n = recvfrom(sockfd, (char *)word, MAXLINE,
+                                MSG_WAITALL, ( struct sockaddr *) &servaddr,
+                                &len);
+                		word[n] = '\0';
+
+				for(int i = 0; i < 8; i++)
+                		{
+                		        guessWord[i] = word[i + 3];
+        		        }
+	
+                		printf("%s\n",guessWord);
+
+				guesses++;
+			}
+		}
+		close(sockfd);
+	}
+	else
+	{
+		close(sockfd);
+	}
+
+	return 0; 
+} 
+
---
+++ pa3/hangman_server.c
@@ -0,0 +1,163 @@
+// Server side implementation of UDP client-server model 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <time.h>
+#define PORT	 8080 
+#define MAXLINE 1024 
+
+// Driver code 
+
+char* choose_random_word(const char *filename) {
+    FILE *f;
+    size_t lineno = 0;
+    size_t selectlen;
+    char selected[256]; /* Arbitrary, make it whatever size makes sense */
+    char current[256];
+    selected[0] = '\0'; /* Don't crash if file is empty */
+
+    f = fopen(filename, "r"); /* Add your own error checking */
+    int randomLine = rand() % 15;
+    //printf("%d\n",randomLine);
+    while (fgets(current, sizeof(current), f)) {
+        if ( randomLine == lineno) {
+            strcpy(selected, current);
+	    lineno++;
+        }
+	else
+	{
+		lineno++;
+	}
+    }
+    fclose(f);
+    selectlen = strlen(selected);
+    if (selectlen > 0 && selected[selectlen-1] == '\n') {
+        selected[selectlen-1] = '\0';
+    }
+    return strdup(selected);
+}
+
+int main() {
+       	srand(time(0));	
+	int sockfd; 
+	char buffer[MAXLINE]; 
+	char* word = "        ";
+	struct sockaddr_in servaddr, cliaddr;
+
+		
+	
+	// Creating socket file descriptor 
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+		perror("socket creation failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	memset(&cliaddr, 0, sizeof(cliaddr)); 
+	
+	// Filling server information 
+	servaddr.sin_family = AF_INET; // IPv4 
+	servaddr.sin_addr.s_addr = INADDR_ANY; 
+	servaddr.sin_port = htons(PORT); 
+	
+	// Bind the socket with the server address 
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
+			sizeof(servaddr)) < 0 ) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	
+	int len, n; 
+
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+				MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+				&len); 
+	buffer[n] = '\0'; 
+	
+	//char* message = "applesauce";
+
+     	//sendto(sockfd, (const char *)message, strlen(message),  
+        //MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+        //    len); 
+	
+	char* wordToGuess = choose_random_word("hangman_words.txt");
+	printf("%s\n",wordToGuess);	
+	int wordLength = strlen(wordToGuess);
+	wordLength = wordLength + 3;
+	char message[] = "           ";
+	int guesses = 0;
+	
+	int asciiWordlength = strlen(wordToGuess) + 48;
+	
+	message[0] = '0';
+	message[1] = (char )(asciiWordlength);
+	message[2] = (char )48;
+	
+	//printf("%s/n",message);
+	
+	for(int i = 3; i < wordLength;i++)
+	{
+		message[i] = (char )95;
+	}
+	message[wordLength] = '\0';
+	
+
+	for(int i = 0; i < wordLength;i++)
+	{	
+		//printf("%c",message[i]);
+		//word[i] = message[i];
+		
+	}
+	printf("\n");
+	
+
+	//word[wordLength] = '\0';
+
+	printf("%s\n",message);
+
+	sendto(sockfd, (const char *)message, sizeof(message),
+                MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+                        len);
+	char* incorrect = "   Incorrect\n";
+	int correct = 0;
+	while(1)
+	{
+		int len, n;
+		n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                                MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                                &len);
+                        buffer[n] = '\0';
+                        printf("Client: Gussed %s\n", buffer);	
+		for(int i = 0; i < strlen(wordToGuess); i++)
+		{
+			if(wordToGuess[i] == buffer[0])
+			{
+				message[i+3] = buffer[0];
+				correct = 1;	
+			}
+		}
+		if (correct){
+			sendto(sockfd, (const char *)message, sizeof(message),
+                        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+                        len);
+		}
+		else
+		{
+			sendto(sockfd, (const char *)incorrect, sizeof(incorrect),
+                        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+                        len);
+		}
+	}
+
+
+
+	return 0; 
+} 
+
